fd56dd06d04c4a3b267b37ab0b63380d50886c6e
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-06-22 11:45:04 +0530

    Initialized project using react vite + TS + MUI and install necessary dependancies

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+// import ProtectedRoute from "./routes/ProtectedRoute";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={
+              <Dashboard />
+              // <ProtectedRoute>
+              //   <Dashboard />
+              // </ProtectedRoute>
+            }
+          />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme.tsx";
+import { Provider } from 'react-redux';
+import { store } from './features/store/store.ts';
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+    </Provider>
+  </StrictMode>
+);
